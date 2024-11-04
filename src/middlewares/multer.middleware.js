e23e4ef979cbfc27f624156a627db588f892a76e
vanshtyagi04 <vanshtyagi4526@gmail.com>
2024-11-04 23:46:53 +0530

    added middlewares and fixed the user model

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = /jpg|jpeg|png|gif/; 
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedTypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        return cb(null, true);
+    } else {
+        cb(new Error("Only images are allowed!"), false);
+    }
+};
+
+export const upload = multer({ 
+    storage, 
+    limits: { fileSize: 1024 * 1024 * 5 },
+    fileFilter 
+});
