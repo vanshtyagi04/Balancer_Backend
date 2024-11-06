78d35102e6a1a57a9838447047625538e4d3c19f
vanshtyagi04 <vanshtyagi4526@gmail.com>
2024-11-06 19:01:49 +0530

    additional functionalities and notification model

--- src/models/notification.model.js
+++ src/models/notification.model.js
@@ -2,7 +2,22 @@ import mongoose, { Schema } from "mongoose";
 
 const notificationSchema = new Schema(
     {
-       
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+             type: String,
+             required: true,
+        },
+        task: {
+            type: Schema.Types.ObjectId,
+            ref: "Task" 
+        },
+        groupName: {
+            type: String,
+            required: true
+        }
     },
     {
         timestamps: true
